a29d3a13c2483193d87cf16c4e79e94a6752e38a
Diana Carolina Badas <33879699+DianaBaRo@users.noreply.github.com>
2020-03-19 14:14:40 +0000

    Update solution

--- solution.js
+++ solution.js
@@ -26,22 +26,34 @@ function readLine() {
 
 // Complete the countingValleys function below.
 function countingValleys(n, s) {
-    let level = 0;
-    let prevLevel = 0;
-    let valleys = 0;
+  const min = 2;
+  const max = 1000000;
+  let isInValley = false;
+  let valleys = 0;
+  s = (typeof s === "string") ? s.split('') : s;
+ 
+  if (s.length >= min
+    && s.length <= max
+    && n === parseInt(n, 0)
+    && n >= min
+    && n <= max 
+    && n === s.length) {
+          
+    s.map(steps => ((steps === "U") ? 1 : -1))
+         .reduce((prev, next) => {
+              if (prev < 0 && !isInValley) {
+                         isInValley = true;
+              }
+              if ((prev + next) === 0 && isInValley) {
+                   valleys++;
+                   isInValley = false;
+              }
 
-    for(let i=0; i < n; i++) {
-        if (s[i] == "U"){
-            prevLevel = level;
-            level++;
-        } else if (level == 0 && prevLevel == -1) {
-            valleys++
-        } else {
-            prevLevel = level;
-            level--;
-        };
-    };
-    return valleys;
+              return prev + next;    
+         });
+   } 
+     
+   return valleys; 
 }
 
 function main() {
